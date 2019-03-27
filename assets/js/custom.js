0e8a0261081e3decbee14665d1ad2e1c8c4b0221
umeshnaik <umesh55ckdy@gmail.com>
2019-03-27 12:09:34 +0530

    Fixed blog menu highlight on recent posts section (#10)

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -4,6 +4,10 @@
     for (const link of links) {
         if (currentUrl.indexOf(link.href) !== -1) {
             link.classList.add('active')
+        } else {
+            if(currentUrl.indexOf("blog") !== -1 && link.href.indexOf("#posts")!== -1) {
+                link.classList.add('active')
+            }
         }
     }
 }())
